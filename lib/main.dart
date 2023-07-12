cd2fb31077bdd01866e7f929734c0f84fa96b8e0
xasanovgo <akromjonxasanov23@gmail.com>
2023-07-12 18:02:00 +0500

    fake store api

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:api_default_project/ui/splash/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'data/local/storage_repository.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await StorageRepository.getInstance();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
